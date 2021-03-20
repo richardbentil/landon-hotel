7b9673be5a12340a4ec715ffa90bb6c6f7630b87
richard <richard@hubtel.com>
2021-03-20 10:03:02 -0300

    add dynamic data from json files

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import menuLinks from './data/menu-links.json'
+
+const Header = () => {
+  return (
+    <>
+      <header id="intro">
+        <article className="fullheight">
+          <div className="hgroup">
+            <h1>Landon Hotel</h1>
+            <h2>West London</h2>
+            <p>
+              <a href="#welcome">
+                <img
+                  src="https://landonhotel.com/images/misc/arrow.png"
+                  alt="down arrow"
+                />
+              </a>
+            </p>
+          </div>
+        </article>
+
+        <nav id="nav">
+          <div className="navbar">
+            <div className="brand">
+              <a href="#welcome">
+                Landon <span>Hotel</span>
+              </a>
+            </div>
+            <ul>
+              {menuLinks.map((link, i) => {
+              return (
+                <>
+                  <li key={i}>
+                    <a
+                      className={`icon ${link.class}`}
+                      href={`#${link.href}`}>
+                      <span>{link.text}</span>
+                    </a>
+                  </li>
+                </>
+              );
+              }
+              )}
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header
